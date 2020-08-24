b2efcf52d4b0495a72f1e67d8df159afaffc91da
Amundeep Singh Dhaliwal <64365442+Amundeep-Dhaliwal@users.noreply.github.com>
2020-08-24 19:55:14 +0100

    Update math.py

--- math.py
+++ math.py
@@ -21,6 +21,18 @@ print(str(int(round(math.degrees(math.atan2(AB,BC)))))+u"\N{DEGREE SIGN}")
 for i in range(int(input())):
     print([1,121,12321,1234321,123454321,12345654321,1234567654321,123456787654321,12345678987654321,12345678987654321][i])
 
+# the code below prints the right angle triangle of numbers upside down
+
+countDownFrom = int(input())
+while countDownFrom > 1:
+    for start, stop, step in ((1,countDownFrom,1), (countDownFrom,0,-1)):
+        for number in range(start, stop, step):
+            print(number, end = '')
+    print('')
+    countDownFrom -= 1
+    if countDownFrom == 1:
+        print(1)
+
 # mod divmod 
 
 n = int(input())
