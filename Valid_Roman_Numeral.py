1c48007af1ba0f4ac2e55ca76ca74f0c3d0f8ba2
Amundeep Singh Dhaliwal <64365442+Amundeep-Dhaliwal@users.noreply.github.com>
2020-08-27 19:37:01 +0100

    Delete Valid_Roman_Numeral.py

--- Valid_Roman_Numeral.py
+++
@@ -1,9 +0,0 @@
-# This function can be used to check whether a roman numeral between 1 and 4000 is valid
-
-import re
-def roman_numeral_check(n):
-    thousand = 'M{0,3}'
-    hundred = '(C[MD]|D?C{0,3})'
-    ten = '(X[CL]|L?X{0,3})'
-    digit= '(I[VX]|V?I{0,3})'
-    print(bool(re.match(thousand+hundred+ten+digit+'$', n)))
